165fc7a071cf56a305df205c10a013c6315f6ef1
ShootingersNew <lebedevseergey@gmail.com>
2024-12-21 05:54:22 +0300

    feat: config for microfrontend usage

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,28 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import federation from "@originjs/vite-plugin-federation";
 
 // https://vite.dev/config/
 export default defineConfig({
+  preview: {
+    port: 5005,
+  },
+  build: {
+    target: 'esnext'
+  },
   plugins: [
     vue(),
     vueJsx(),
     vueDevTools(),
+    federation({
+      name: "meduza-pet-header",
+      filename: "meduzaHeader.js",
+      exposes: {
+        "./App": "./src/App.vue",
+      },
+      shared: ["vue"],
+    }),
   ],
   resolve: {
     alias: {
