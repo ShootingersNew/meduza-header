3b9b822d982ab3da7dda54bf0a55056590348cb0
ShootingersNew <lebedevseergey@gmail.com>
2024-12-21 10:30:32 +0300

    fix: linter

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import federation from "@originjs/vite-plugin-federation"
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import { defineConfig } from 'vite'
+// @ts-expect-error See https://github.com/gxmari007/vite-plugin-eslint/issues/79
+import eslintPlugin from 'vite-plugin-eslint'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import federation from "@originjs/vite-plugin-federation";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -15,6 +17,7 @@ export default defineConfig({
     target: 'esnext'
   },
   plugins: [
+    eslintPlugin,
     vue(),
     vueJsx(),
     vueDevTools(),
