f85eefabaf8ab098f6fcc2b1dcb18c2b0f46b0e0
ShootingersNew <lebedevseergey@gmail.com>
2025-01-15 09:30:52 +0300

    feat: update pckg and add some styles from ui-kit

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <template>
-  <div>header</div>
+  <div>
+    <div>header</div>
+    <div>kek</div>
+  </div>
 </template>
 
 <script lang="ts">
+import 'meduza-pet-ui-kit/styles'
+import 'meduza-pet-ui-kit/variables'
 import { defineComponent } from 'vue'
 
 export default defineComponent({
   name: 'App',
-  setup() {
-    return {}
-  },
 })
 </script>
