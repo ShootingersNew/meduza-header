6d8dfa4f52830206bb682cc4b280e86bf7cb8391
ShootingersNew <lebedevseergey@gmail.com>
2025-01-30 15:10:34 +0300

    feat: add libs

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createI18n } from 'vue-i18n'
 
+const i18n = createI18n({
+  // something vue-i18n options here ...
+})
 const app = createApp(App)
-
+app.use(i18n);
 app.use(createPinia())
 
 app.mount('#app')
